7cfe0e0b84c958f8f79e2faff1ccd54cb0e620f8
neeldarji12 <neeldarji1431@gmail.com>
2026-02-09 08:41:16 +0530

    Add action creators for ADD, DEL, SET_EDIT, and EDIT

--- Action.js
+++ Action.js
@@ -1,1 +1,19 @@
+export const myAction = (payload) => ({
+    type: "ADD",
+    payload
+})
 
+export const delAction = (payload) => ({
+    type: "DEL",
+    payload
+})
+
+export const setEditAction = (index, data) => ({
+    type: "SET_EDIT",
+    payload: { index, data }
+})
+
+export const editAction = (payload) => ({
+    type: "EDIT",
+    payload
+})

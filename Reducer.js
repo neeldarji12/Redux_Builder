6d5aed2138cf7cb52d7651d28bb84cb503e17ae7
neeldarji12 <neeldarji1431@gmail.com>
2026-02-09 08:43:31 +0530

    Update Reducer.js

--- Reducer.js
+++ Reducer.js
@@ -1,1 +1,42 @@
 
+const initialState = {
+    users: [],
+    editIndex: null,
+    editData: { name: "", email: "" }
+}
+
+export const myReducer = (state = initialState, action) => {
+    if (action.type === "ADD") {
+        return {
+            ...state,
+            users: [...state.users, action.payload]
+        }
+    }
+
+    if (action.type === "DEL") {
+        return {
+            ...state,
+            users: state.users.filter((_, i) => i !== action.payload)
+        }
+    }
+
+    if (action.type === "SET_EDIT") {
+        return {
+            ...state,
+            editIndex: action.payload.index,
+            editData: action.payload.data
+        }
+    }
+
+    if (action.type === "EDIT") {
+        return {
+            users: state.users.map((el, i) =>
+                i === state.editIndex ? action.payload : el
+            ),
+            editIndex: null,
+            editData: { name: "", email: "" }
+        }
+    }
+
+    return state
+}
